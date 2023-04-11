61fb566d75400d03dc77eb6ad637233c4aa29543
e2203080 <e2203080@edu.vamk.fi>
2023-04-11 17:21:44 +0300

    muutos

--- Game_of_life.c
+++ Game_of_life.c
@@ -1,1 +1,84 @@
- program under here 
+/***************************************************************************
+ *   Copyright (C) $2023$ by $Koskela Jaakko$   *
+ *   $koskela63z@gmail.com$   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/*********************************************************************
+
+ 1.  NAME
+     game of life version 1.0
+
+ 2.  DESCRIPTION
+     game of life simple version
+
+ 3.  VERSIONS
+       Original:
+         1.0.2023 / Jaakko Koskela
+
+       Version history:
+        1.0 
+**********************************************************************/
+
+/*-------------------------------------------------------------------*
+ *    HEADER FILES                                                    *
+ *--------------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/*-------------------------------------------------------------------*
+ *    GLOBAL VARIABLES AND CONSTANTS                                  *
+ *--------------------------------------------------------------------*/
+/* Control flags */
+
+/* Global constants */
+
+
+/* Global variables */
+
+/* Global structures */
+
+/*-------------------------------------------------------------------*
+ *    FUNCTION PROTOTYPES                                             *
+ *--------------------------------------------------------------------*/
+
+
+/*********************************************************************
+ *    MAIN PROGRAM                                                      *
+ **********************************************************************/
+
+
+
+/* end of main */
+
+/*********************************************************************
+ *    FUNCTIONS                                                       *
+ **********************************************************************/
+
+/*********************************************************************
+    F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: 
+ DESCRIPTION: 
+    Input:
+    Output: 
+  Used global variables:
+ REMARKS when using this function:
+*********************************************************************/
+
