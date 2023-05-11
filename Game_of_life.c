2b9e9f60a7ba9ec897471e3cb24a937a64c5e8cf
e2203080 <e2203080@edu.vamk.fi>
2023-05-11 20:06:11 +0300

    testipäivitysd

--- Game_of_life.c
+++ Game_of_life.c
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) $2023$ by $Koskela Jaakko$   *
- *   $koskela63z@gmail.com$   *
+ *   Copyright (C) $2023$ by $Koskela Jaakko$                              *
+ *   $koskela63z@gmail.com$                                                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
